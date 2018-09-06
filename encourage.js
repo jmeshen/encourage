c0d825a6c4192e8ae115d8427f0d04a6fa9e993a
jmeshen <jamie@pager.com>
2018-09-05 23:45:29 -0400

    refactor: cleanup functions and vars

--- encourage.js
+++ encourage.js
@@ -1,23 +1,11 @@
-let nameInput;
-let name;
+let NAME;
+const nameInput = document.querySelector('form').name;
 let intervalId;
 let started = false;
+
 function focusInput() {
-  nameInput = document.querySelector('form').name;
   nameInput.focus();
 }
-function ready(fn) {
-  if (
-    document.attachEvent
-      ? document.readyState === 'complete'
-      : document.readyState !== 'loading'
-  ) {
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-// ready(focusInput);
 
 function sendNotification(text) {
   const options = {
@@ -27,23 +15,21 @@ function sendNotification(text) {
   const n = new Notification('Encouragement', options);
   window.setTimeout(n.close.bind(n), 5000);
 }
-function generateMessage(n) {
+function generateMessage() {
   const messages = [
-    `You can do it, ${name}! 💪`,
+    `You can do it, ${NAME}! 💪`,
     'Breathe, you got this! 🤗',
-    `Hey ${name}, you're awesome! 😊`,
-    `KEEP CRUSHING IT ${name}! 👊`,
-    `You da you da best ${name} 💯`,
+    `Hey ${NAME}, you're awesome! 😊`,
+    `KEEP CRUSHING IT ${NAME.toUpperCase()}! 👊`,
+    `You da you da best ${NAME} 💯`,
     'YASSSS KEEP IT UP! 🙌',
-    `You are excellence, ${name}! 👍`,
+    `You are excellence, ${NAME}! 👍`,
     'Keep calm and go go go 👑',
     'Do. Or do not. There is no try. 🤓',
     "Don't watch the clock; keep going. ⏰",
     "⭐ You're a star! ⭐",
   ];
-  return n === 0
-    ? messages[0]
-    : messages[Math.floor(Math.random() * messages.length)];
+  return messages[Math.floor(Math.random() * messages.length)];
 }
 function startNotificationInterval() {
   let randomMsg = generateMessage(0);
@@ -55,21 +41,29 @@ function startNotificationInterval() {
   started = true;
 }
 
+function isBrowserCompatible() {
+  if (!('Notification' in window)) {
+    return false;
+  }
+  return true;
+}
+
 function greetingNotification() {
   // Let's check if the browser supports notifications
-  if (!('Notification' in window)) {
-    alert("Sorry, it looks like your browser does not support notifications 😞, if you're on mobile try on your computer 👩‍💻👨‍💻",);
-    return false;
-  } else if (Notification.permission === 'granted') {
-    sendNotification(`Hi there ${name}! 👋`);
-    startNotificationInterval();
-    return true;
-  } else if (Notification.permission !== 'denied') {
-    Notification.requestPermission((permission) => {
+  if (!isBrowserCompatible()) {
+    alert(
+      "Sorry, it looks like your browser does not support notifications 😞, if you're on mobile try on your computer 👩‍💻👨‍💻",
+    );
+  } else {
+    Notification.requestPermission().then((permission) => {
       if (permission === 'granted') {
-        sendNotification(`Hi there ${name}! 👋`);
+        sendNotification(`Hi there ${NAME}! 👋`);
         startNotificationInterval();
-        return true;
+        document.querySelector(
+          '#successMessage',
+        ).innerHTML = `awesome, you're all set ${NAME} 💯<br/><br/>keep this tab open in the background and you'll receive <br/>encouraging notifications every so often ⭐ <br/><br/><button id="triggerNotification">🚨 emergency notification</button><br/><br/><button id="stopLink">🛑 stop the madness</button>`;
+      } else {
+        document.querySelector('#successMessage').innerHTML =          'Sorry, you need to Allow notifications if you want to be encouraged!';
       }
     });
   }
@@ -80,25 +74,27 @@ function start(e) {
   if (started) {
     return;
   }
-  name = nameInput.value;
+  NAME = nameInput.value;
   nameInput.blur();
-  const canSend = greetingNotification(name);
-  if (!canSend) {
-    return;
-  }
-  document.querySelector('#successMessage',).innerHTML = `awesome, you're all set ${name} 💯<br/><br/>keep this tab open in the background and you'll receive <br/>encouraging notifications every so often ⭐ <br/><br/><button id="triggerNotification">🚨 emergency notification</button><br/><br/><button id="stopLink">🛑 stop the madness</button>`;
+  greetingNotification();
 }
 
-window.onload = () => {
-  focusInput();
-  document.querySelector('body').addEventListener('click', (event) => {
-    if (event.target.id === 'stopLink') {
-      window.clearInterval(intervalId);
-      document.querySelector('#successMessage').innerHTML = '';
-      started = false;
-    }
-    if (event.target.id === 'triggerNotification') {
-      sendNotification(generateMessage());
-    }
-  });
-};
+focusInput();
+
+document.querySelector('body').addEventListener('click', (event) => {
+  if (event.target.id === 'stopLink') {
+    window.clearInterval(intervalId);
+    document.querySelector('#successMessage').innerHTML = '';
+    started = false;
+  }
+  if (event.target.id === 'triggerNotification') {
+    sendNotification(generateMessage());
+  }
+});
+
+document.querySelector('.form').addEventListener('submit', (event) => {
+  start(event);
+});
+
+// TODO: cleanup stop logic and appending to DOM
+// change Start to Stop button
