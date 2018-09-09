0d932c3686d237e217f63da5c953da9d5ecdec0d
jmeshen <jamie@pager.com>
2018-09-09 03:04:51 -0400

    refactor: es module and structure

---
+++ js/messages.js
@@ -0,0 +1,15 @@
+export default function messages(name) {
+  return [
+    `You can do it, ${name}! 💪`,
+    'Breathe, you got this! 🤗',
+    `Hey ${name}, you're awesome! 😊`,
+    `KEEP CRUSHING IT ${name.toUpperCase()}! 👊`,
+    `You da you da best ${name} 💯`,
+    'YASSSS KEEP IT UP! 🙌',
+    `You are excellence, ${name}! 👍`,
+    'Keep calm and go go go 👑',
+    'Do. Or do not. There is no try. 🤓',
+    "Don't watch the clock; keep going. ⏰",
+    "⭐ You're a star! ⭐"
+  ];
+}
